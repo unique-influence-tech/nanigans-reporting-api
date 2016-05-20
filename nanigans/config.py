87c3cfae1253d9038c3c7bb5df5b99168199cbb3
kennes913 <kennes913@gmail.com>
2016-05-20 15:47:39 -0500

    add config.py

---
+++ nanigans/config.py
@@ -0,0 +1,11 @@
+
+# Nanigans Reporting API 2.0 Configuration
+
+NAN_CONFIG = {'token':'', 
+			  'site':''}
+
+DATABASE_CONFIG = {'user':'', 
+				   'password':'', 
+				   'database':'',
+				   'host':''}
+
