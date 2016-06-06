dc76f84429645f276da45c8615d7dbc45aadaab9
kennes913 <kennes913@gmail.com>
2016-06-06 16:05:29 -0500

    updated config file

--- nanigans/config.py
+++ nanigans/config.py
@@ -1,8 +1,7 @@
-from .utils import generate_token
 
 NAN_CONFIG = {'site':'',
-			  'username':'',
-			  'password':''}
+			 'username':'',
+			 'password':''}
 
-NAN_CONFIG['token'] = generate_token(NAN_CONFIG['username'], NAN_CONFIG['password'], NAN_CONFIG['site'])
+
 
